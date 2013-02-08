08d8b533108c8e0e5a58a1ae5d4cc73ee3b0b251
fsbruva <fsbruva@yahoo.com>
2013-02-07 23:45:32 -0500

    Update thebrig_install.sh

--- thebrig_install.sh
+++ thebrig_install.sh
@@ -11,3 +11,11 @@ fetch https://github.com/fsbruva/thebrig/archive/master.zip
 # the git nonsense
 tar -xvf master.zip --exclude='.git*' --strip-components 1
 
+# Determine the current directory
+# Method adapted from user apokalyptik at http://hintsforums.macworld.com/archive/index.php/t-73839.html
+STAT=$(procstat -f $$ | grep -E "/"$(basename $0)"$")
+FULL_PATH=$(echo $STAT | sed -r s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+
+# Place the path (of the current directory) within a file for the intial
+# run of the extension
+cat FULL_PATH > /tmp/thebrig.tmp
