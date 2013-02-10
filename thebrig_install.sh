d5c828dd664d0b7866c694acfd16321f552fa9aa
fsbruva <fsbruva@yahoo.com>
2013-02-10 09:43:52 -0500

    Repaired install script

--- thebrig_install.sh
+++ thebrig_install.sh
@@ -12,12 +12,14 @@ fetch https://github.com/fsbruva/thebrig/archive/master.zip
 tar -xvf master.zip --exclude='.git*' --strip-components 1
 
 # Determine the current directory
-# Method adapted from user apokalyptik at http://hintsforums.macworld.com/archive/index.php/t-73839.html
+# Method adapted from user apokalyptik at
+# http://hintsforums.macworld.com/archive/index.php/t-73839.html
 STAT=$(procstat -f $$ | grep -E "/"$(basename $0)"$")
 FULL_PATH=$(echo $STAT | sed -r s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+BRIG_ROOT=$(dirname $FULL_PATH | sed 's|/thebrig_install.sh||')
 
 # Place the path (of the current directory) within a file for the intial
 # run of the extension
-cat FULL_PATH > /tmp/thebrig.tmp
+echo $BRIG_ROOT > /tmp/thebrig.tmp
 
 bin/sh FULL_PATH/bin/jail_start.sh
