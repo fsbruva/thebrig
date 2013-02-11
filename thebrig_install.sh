5a3e4283e6a3bdc9041ea6deebd134826f94466f
fsbruva <fsbruva@yahoo.com>
2013-02-11 11:30:02 -0500

    Fixed script

--- thebrig_install.sh
+++ thebrig_install.sh
@@ -22,4 +22,4 @@ BRIG_ROOT=$(dirname $FULL_PATH | sed 's|/thebrig_install.sh||')
 # run of the extension
 echo $BRIG_ROOT > /tmp/thebrig.tmp
 
-bin/sh FULL_PATH/bin/jail_start.sh
+/bin/sh $BRIG_ROOT/bin/jail_start.sh
