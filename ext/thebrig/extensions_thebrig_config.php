07d87955207ae1e7f801ccb574d0c9552bcf3d45
Matthew Kempe <fsbruva@yahoo.com>
2013-02-12 12:06:39 -0500

    Force initial population.

--- ext/thebrig/extensions_thebrig_config.php
+++ ext/thebrig/extensions_thebrig_config.php
@@ -28,6 +28,7 @@ if ( ( !isset( $config['thebrig']['rootfolder'] ) ) && file_exists( '/tmp/thebri
 	// If the thing pulled from the .tmp file is an actual directory, do some stuff
 	if ( is_dir( $config['thebrig']['rootfolder'] ) ) {
 		write_config();		// write the config so it survives reboot
+		thebrig_populate( $config['rootfolder'] , $config['thebrig']['rootfolder'] );
 		unlink_if_exists("/tmp/thebrig.tmp");  // deletes the .tmp file (if it was there)
 	}
 	else {
