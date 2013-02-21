c6b600013140d5d0941e8a9a0192fca09ad11d39
Matthew Kempe <fsbruva@yahoo.com>
2013-02-21 15:28:54 -0500

    Jail_start works from commandline

--- conf/ext/thebrig/jail_start.sh
+++ conf/ext/thebrig/jail_start.sh
@@ -2,9 +2,11 @@
 <?php
 /*
 This is only fo reference. I will check it and remove this message,when OK.  It worked from webgui now, but I not checked from command line
-jail_start.php
+jail_start.php.
+This function works from the command line, but the #! doesn't function appropriately. It says there is 
+no file specified. However, calling it: php-cgi -f jail_start.sh workd just fine.
 */
-include ("config.inc");
-include ("ext/thebrig/functions.inc");
+require_once ("/etc/inc/config.inc");
+require_once ("{$config['thebrig']['rootfolder']}/conf/ext/thebrig/functions.inc");
 write_rcconflocal ();
 ?>
