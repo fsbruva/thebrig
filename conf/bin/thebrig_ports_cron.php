9570b66db3c7fdc232cffd7cae72b6b4bfaadf94
Matthew Kempe <fsbruva@yahoo.com>
2013-05-29 14:44:17 -0400

    Relocated sleep
    Placed the sleep outside the if statement. This will carry out the sleep
    prior to attempting to build out the list of binaries.

--- conf/bin/thebrig_ports_cron.php
+++ conf/bin/thebrig_ports_cron.php
@@ -7,10 +7,10 @@ $brig_root = $config['thebrig']['rootfolder'];
 require_once ("/usr/local/www/ext/thebrig/functions.inc");
 
 $brig_port_db = $brig_root . "conf/db/ports/";
+sleep ( rand( 0 , 3600 ) );
 
 $brig_update_ready = thebrig_update_prep();
 if ( $brig_update_ready == 0 ) {
-	sleep ( rand( 0 , 3600 ) );
 	$response = thebrig_portsnap($brig_root . "conf/ports", $brig_root . "conf/db/ports", $brig_root . "conf/portsnap.conf", "Fetch");
 }
 
