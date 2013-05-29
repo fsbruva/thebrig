06a95b9510994946ad351826fd89695b485bf04a
Matthew Kempe <fsbruva@yahoo.com>
2013-05-29 14:48:10 -0400

    Relocated sleep, syntax fixes and logic fix
    Sleep moved outside if statement.
    
    The location to store files was not being declared in the script (it
    works just fine on the website). This meant the basejail's storage
    location was now being made available to the freebsd-update script.
    
    Line 24 had an open curly brace but no closure.
    
    Line 44 evaluated an erroneous variable to test if the template jail had
    been filled. Instead, the presence of the /usr directory is used to
    check if the template jail should have the update tasks carried out.

--- conf/bin/thebrig_update_cron.php
+++ conf/bin/thebrig_update_cron.php
@@ -6,27 +6,29 @@ require_once ("/etc/inc/config.inc");
 $brig_root = $config['thebrig']['rootfolder'];
 require_once ("/usr/local/www/ext/thebrig/functions.inc");
 
-
+$brig_update_db = $brig_root . "conf/db/freebsd-update/";
 array_sort_key($config['thebrig']['content'], "jailno");
 $a_jail = &$config['thebrig']['content'];
+sleep ( rand( 0 , 3600 ) );
 $brig_update_ready = thebrig_update_prep();
 $basedir_list = array();
 $workdir_list = array();
 $conffile_list = array();
 $base_selected = false;
 if ( $brig_update_ready == 0 ) {
-	sleep ( rand( 0 , 3600 ) );
+	
 	// We will be building arrays of base directories, working directories and config files
 	// to be used for the fetch operations.
 	foreach ( $a_jail as $my_jail ) {
 		// now we need to prep for the actual updating
 		// This if gets entered the jail is checked and is fullsized
-		if  ( $my_jail['type'] == 'full' ){
+		if  ( $my_jail['type'] == 'full' ) 
 			$conffile_list[] = $brig_root . "conf/freebsd-update.conf";
 		else {
 			// The current jail is slim, so we have to use a special conf that excludes the shared basejail
 			$conffile_list[] = $brig_root . "conf/freebsd-update_thin.conf";
 			$base_selected = true;
+			
 		} // end of slim + basejail
 		$basedir_list[]=$my_jail['jailpath'];
 		$workdir_list[]=$my_jail['jailpath'] . "var/db/freebsd-update/";
@@ -41,7 +43,7 @@ if ( $brig_update_ready == 0 ) {
 	} // end of basejail selected
 
 	// We need to take care of the template jail!
-	if ( $template_selected ){
+	if ( is_dir ( $config['thebrig']['template'] . "usr/" )){
 		$template_dir = $config['thebrig']['template'];
 		$basedir_list[]=$template_dir;
 		$workdir_list[]=$template_dir . "var/db/freebsd-update/";
