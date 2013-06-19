2a62676820368ba98aacfca4877a3a1fbf9f0c25
fsbruva <fsbruva@yahoo.com>
2013-06-19 15:24:42 -0300

    Remove default activation of ports.
    The default installation sets the 'ports' key to false. However, the fstab creation script checks if the key exists, since true and false cannot be saved to the XML file. Now there is no default ports key until the testing branch gets merged in.

--- conf/ext/thebrig/extensions_thebrig_edit.php
+++ conf/ext/thebrig/extensions_thebrig_edit.php
@@ -94,7 +94,7 @@ if (isset($uuid) && (FALSE !== ($cnid = array_search_ex($uuid, $a_jail, "uuid"))
 	$pconfig['force_blocking'] = $a_jail[$cnid]['force_blocking'];
 	$pconfig['zfs_datasets'] = $a_jail[$cnid]['zfs_datasets'];
 	$pconfig['fib'] = $a_jail[$cnid]['fib'];
-	$pconfig['ports'] = ( isset($a_jail[$cnid]['ports']) ) ? $a_jail[$cnid]['ports'] : false ;
+	$pconfig['ports'] = ( isset($a_jail[$cnid]['ports']) ) ? true : false ;
 	// By default, when editing an existing jail, path and name will be read only.
 	$path_ro = true;
 	$name_ro = true;
@@ -141,7 +141,6 @@ else {
 	$pconfig['force_blocking'] = "";
 	$pconfig['zfs_datasets'] = "";
 	$pconfig['fib'] = "";
-	$pconfig['ports']=false;
 	$path_ro = false;
 	$name_ro = false;
 }
