460799b8c808df854850caf2cc677933ae265d70
alexey1234 <alexeykr68@gmail.com>
2014-01-20 08:03:50 +0200

    Update extensions_thebrig.php

--- conf/ext/thebrig/extensions_thebrig.php
+++ conf/ext/thebrig/extensions_thebrig.php
@@ -169,7 +169,10 @@ var auto_refresh = setInterval(
 	<tr><td class="tabnavtbl">
 		<ul id="tabnav">
 			<li class="tabact"><a href="extensions_thebrig.php"><span><?=_THEBRIG_JAILS;?></span></a></li>
-			<li class="tabinact"><a href="extensions_thebrig_update.php"><span><?=_THEBRIG_UPDATES;?></span></a></li>
+			<?php If (!empty($config['thebrig']['content'])) { 
+			$thebrigupdates=_THEBRIG_UPDATES;
+			echo "<li class=\"tabinact\"><a href=\"extensions_thebrig_update.php\"><span>{$thebrigupdates}</span></a></li>";
+			} else {} ?>
 			<li class="tabinact"><a href="extensions_thebrig_tarballs.php"><span><?=_THEBRIG_MAINTENANCE;?></span></a></li>
 			<li class="tabinact"><a href="extensions_thebrig_log.php"><span><?=gettext("Log");?></span></a></li>
 					</span> </a>
