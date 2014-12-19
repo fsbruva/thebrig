1c93a652f743a12be156e5e7cb9b27137a9eeebe
fsbruva <fsbruva@yahoo.com>
2014-12-18 22:58:22 -0900

    Restructuring of the symbolic links for the extension to the webroot.
    Instead of copying the entire ext/thebrig folder and then linking it,
    we just link directly.

--- conf/bin/thebrig_start.php
+++ conf/bin/thebrig_start.php
@@ -1,19 +1,41 @@
 #!/usr/local/bin/php-cgi -f
 <?php
 /*
+ * File name: 	thebrig_start.php
+ * Author: 		Matt Kempe
+ * Date:		Dec 2014
+ * 
+ * Purpose: 	This script is used to prepare the extension for use by
+ * 				Nas4Free's lighttpd webgui server.
+ * 
+ * Variables used:
+ * 
+ * thebrig_ext	a string containing the real storage location of
+ * 				the ext/thebrig folder. It simplifies code.
+ * php_list		An array of all the php files in the ext/thebrig
+ * php_file		Variable used to control a for loop
+ * a_jail		An array of all the jail information from the config.xml
 */
 require_once ("config.inc");
 require_once ("{$config['thebrig']['rootfolder']}conf/ext/thebrig/functions.inc");
 if ( ! copy ( $config['thebrig']['rootfolder']."conf/bin/jail.sh", "/etc/rc.d/jail"))  
 	{ exec ("logger Failed copy rc script");} 
 chmod("/etc/rc.d/jail", 0755);
-exec( "mkdir -p /usr/local/www/ext/thebrig/" );
-exec( "cp {$config['thebrig']['rootfolder']}conf/ext/thebrig/* /usr/local/www/ext/thebrig/");
-$php_list = glob( "/usr/local/www/ext/thebrig/*.php" ); 
+
+// This might be the first extension, so we need to create the folder for it
+exec( "mkdir -p /usr/local/www/ext" );
+// Make life a little easier
+$thebrig_ext = "{$config['thebrig']['rootfolder']}conf/ext/thebrig";
+// Link the entire folder into the extension location
+exec( "ln -s {$thebrig_ext} /usr/local/www/ext/thebrig");
+// Create a list of all the php files that need to be linked into the webroot
+$php_list = glob( "{$thebrig_ext}/*.php" ); 
+// We need to extract just the file name so the symbolic links make sense
 foreach ( $php_list as $php_file ) {
-	$php_file = str_replace( "/usr/local/www/ext/thebrig/" , "", $php_file);
-	if ( is_link ( "/usr/local/www/ext/thebrig/" . $php_file ) ) {	unlink (  "/usr/local/www/ext/thebrig/" . $php_file );	} else {}
-	exec ( "ln -s /usr/local/www/ext/thebrig/{$php_file} /usr/local/www/{$php_file}");
+	// Cut off the prefix to obtain the filename
+	$php_file = str_replace( "{$thebrig_ext}/" , "", $php_file);
+	// Link the real storage location to the webroot
+	exec ( "ln -s {$thebrig_ext}/{$php_file} /usr/local/www/{$php_file}");
 }
 if ( count ( $config['thebrig']['content'] ) > 0 ) {
 	if ( !is_file ( "/etc/rc.conf.local" ) ) {
