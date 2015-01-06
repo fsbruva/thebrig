d3430b034be21d1e3460b415d04fc2e7b1f158eb
alexey1234 <alexeykr68@gmail.com>
2015-01-06 23:07:33 +0200

    try new

--- conf/bin/change_ver.php
+++ conf/bin/change_ver.php
@@ -44,6 +44,7 @@ if (isset($config['thebrig']['gl_statfs']) && is_numeric ($config['thebrig']['gl
 	$handle=fopen("/tmp/upgrademessage.txt", "w");
 	$removemessage = 1;
 	$message = "Warning! Please define parameters for jails manually. \n " ;
+//	$a_jail = &$config['thebrig']['content'];
 // Begin alcatraz
 // backup config in first
 /**	if (is_dir ($config['thebrig']['rootfolder'] )) { */
@@ -53,13 +54,19 @@ if (isset($config['thebrig']['gl_statfs']) && is_numeric ($config['thebrig']['gl
 				exit;
 				} else {}
 		} else  {	exec ("logger Extension homing folder not defined."); exit;} */   /** WOW I'm root on php cli , but I can't copy!! */  
-	
+	$oldthebrigconf = array();
+	$oldthebrigconf['content'] = array();
+	$oldthebrigconf = $config['thebrig'];
+	unset ($config['thebrig']);	
+	$config['thebrig'] = array();
+	$config['thebrig']['content'] = array();
 // conversion
-	unset ($config['thebrig']['sethostname']);
-	unset ($config['thebrig']['unixiproute']);
-	unset ($config['thebrig']['systenv']);
-	$config['thebrig']['gl_statfs'] = 1;
-	foreach ( $config['thebrig']['content'] as $jail) {
+	if (isset ($config['thebrig']['sethostname']))  unset ($oldthebrigconf['sethostname']);
+	if (isset ($config['thebrig']['unixiproute']))  unset ($oldthebrigconf['unixiproute']);
+	if (isset ($config['thebrig']['systenv']))  unset ($oldthebrigconf['systenv']);
+	$oldthebrigconf['gl_statfs'] = 1;
+	
+	foreach ( $oldthebrigconf['content'] as $jail) {
 		$jail['allowedip'] = $jail['if'] ."|". $jail['ipaddr'] ."/". $jail['subnet'] ;
 		unset ($jail['if']);
 		unset ($jail['ipaddr']);
@@ -68,7 +75,7 @@ if (isset($config['thebrig']['gl_statfs']) && is_numeric ($config['thebrig']['gl
 		$jail['cmd'] = array();
 		if (!empty ( $jail['exec_prestart'])) $jail['cmd'][] = "prestart|0|" .  $jail['exec_prestart'];
 		if (!empty ($jail['afterstart0'])) { 
-			$jail['cmd'][] = "afterstart_for_main|0|" .  $jail['afterstart0'];	
+			$jail['cmd'][] = "afterstart_for_main|0|" .  $jail['afterstart0'];
 			if (!empty ($jail['afterstart1']))  $jail['cmd'][] = "afterstart_for_main|1|" .  $jail['afterstart1']; 
 		}
 		if (!empty ($jail['jail_parameters'])) { 
@@ -86,12 +93,15 @@ if (isset($config['thebrig']['gl_statfs']) && is_numeric ($config['thebrig']['gl
 		unset ($jail['attach_params']);
 		unset ($jail['zfs_datasets']);
 		unset ($jail['fib']);
+		
 	} // end foreach jails
 	fclose ($handle);
-	if ($removemessage == 1) exec ("/bin/rm /tmp/upgrademessage.txt");
+	if ($removemessage == 0) exec ("/bin/rm /tmp/upgrademessage.txt");
 	exec ("/bin/rm -rf ".$config['thebrig']['rootfolder']."conf");
 	exec ("/bin/rm /etc/rc.conf.local");
-	exec ("/bin/rm -rf " .$config['thebrig']['rootfolder']."bin");	
+	exec ("/bin/rm -rf " .$config['thebrig']['rootfolder']."bin");
+	
+	$config['thebrig'] = $oldthebrigconf;
 	$config['thebrig']['version'] = $currentversion;
 	write_config();
 	file_put_contents("/tmp/thebrigversion", "upgraded");
