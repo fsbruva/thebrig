c7b899c6176c85a968458e312e36ea09fa99ca97
alexey1234 <alexeykr68@gmail.com>
2015-01-06 18:44:02 +0200

    Begin upgrade.  May be not work, wait for check

--- conf/bin/change_ver.php
+++ conf/bin/change_ver.php
@@ -7,7 +7,8 @@ $workdir = trim($workdir_1[0]);
 $langfile = file("{$workdir}/temporary/conf/ext/thebrig/lang.inc");
 $version_1 = preg_split ( "/VERSION_NBR, 'v/", $langfile[1]);
 $currentversion=substr($version_1[1],0,3);
-if (is_array($config['thebrig'])) {
+if (true === isset($config['thebrig']['gl_statfs']) && (true == is_array($config['thebrig'])) { 	
+	if (is_array($config['thebrig'])) {
 		if ($config['thebrig']['rootfolder']) { 
 			$thebrigrootfolder = $config['thebrig']['rootfolder'];
 			$thebrigversion = $config['thebrig']['version'];
@@ -35,5 +36,67 @@ if (is_array($config['thebrig'])) {
 		}
 	else { $message = "Hello new user, We will install TheBrig now \n";
 	file_put_contents("/tmp/thebrigversion", "installed"); }
-met1 : echo $message;
+	met1 : echo $message;
+}  else { 
+// stop all jails
+	exec ( "/usr/sbin/jls name", $runningjails);
+	if (is_array ($runningjails) && !empty($runningjails)) {foreach ($runningjails as $jail) { exec("/etc/rc.d/jail stop ". $jail);} }
+	$handle=fopen("/tmp/upgrademessage.txt", "w");
+	$removemessage = 1;
+	$message = "Warning! Please define parameters for jails manually. \n " ;
+// Begin alcatraz
+// backup config in first
+	if (is_dir ($config['thebrig']['rootfolder'] )) {
+// Check tag entry, folder, is writable and make backup
+			if ( ! copy ("/conf/config.xml ".$config['thebrig']['rootfolder']."config.xml.backup"))  {
+				exec ("logger Failed copy rc script. TheBrig root folder not writable"); 
+				exit;
+				} else {}
+		} else  {	exec ("logger Extension homing folder not defined."); exit;}
+	$oldthebrigconf = $config['thebrig'];
+// conversion
+	if (isset ($config['thebrig']['sethostname']))  unset ($oldthebrigconf['sethostname']);
+	if (isset ($config['thebrig']['unixiproute']))  unset ($oldthebrigconf['unixiproute']);
+	if (isset ($config['thebrig']['systenv']))  unset ($oldthebrigconf['systenv']);
+	$oldthebrigconf['gl_statfs'] = 1;
+	foreach ($oldthebrigconf['content'] as $jail) {
+		$jail['allowedip'] = $jail['if'] ."|". $jail['ipaddr'] ."/". $jail['subnet'] ;
+		unset $jail['if'];
+		unset $jail['ipaddr'];
+		unset $jail['subnet'];
+		$jail['statfs'] =1;
+		$jail['cmd'] = array();
+		if (!empty ($jail['exec_prestart'])) $jail['cmd'][] = "prestart|0|" .  $oldthebrigconf['exec_prestart'];
+		if (!empty ($jail['afterstart0'])) { 
+			$jail['cmd'][] = "afterstart_for_main|0|" .  $oldthebrigconf['afterstart0'];	
+			if (!empty ($jail['afterstart1']))  $jail['cmd'][] = "afterstart_for_main|1|" .  $oldthebrigconf['afterstart1']; 
+		}
+		if (!empty ($jail['jail_parameters'])) { 
+			$message = "Detected parameters \"" . $jail['jailname']."\" :" . $jail['jail_parameters']."\n" ;
+			fwrite ($handle, $message );
+			$removemessage = 0;
+		}
+		unset ($jail['exec_prestart']);
+		unset ($jail['afterstart1']);
+		unset ($jail['afterstart0']);
+		unset ($jail['extraoptions']);
+		unset ($jail['jail_parameters']);
+		unset ($jail['image']);
+		unset ($jail['image_type']);
+		unset ($jail['attach_params']);
+		unset ($jail['zfs_datasets']);
+		unset ($jail['fib']);
+	} // end foreach jails
+	fclose $handle;
+	if ($removemessage == 1) exec ("rm /tmp/upgrademessage.txt");
+	exec ("rm /etc/rc.d/jail");
+	exec ("/etc/rc.conf.local");
+	exec ("rm /etc/fstab.*");
+	unset ($config['thebrig']);
+	$config['thebrig'] = $oldthebrigconf;
+	$config['thebrig']['version'] = $currentversion;
+	write_config;
+	file_put_contents("/tmp/thebrigversion", "updated");
+	$message = "We upgrade Thebrig \n";
+}
 ?>
