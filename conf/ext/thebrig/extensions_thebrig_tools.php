5dc6a2440a4d03aefc9ef28cc8d8d37932538793
fsbruva <fsbruva@yahoo.com>
2015-02-15 02:33:48 -0900

    Removes errorneous info boxes following initial installation.

--- conf/ext/thebrig/extensions_thebrig_tools.php
+++ conf/ext/thebrig/extensions_thebrig_tools.php
@@ -235,7 +235,12 @@ if ($_POST['submit'] === "Compress") {
 }
 out1: 
 $pgtitle = array(_THEBRIG_EXTN , _THEBRIG_TITLE, "Tools");
-include("fbegin.inc");?>
+include("fbegin.inc");
+// This will evaluate if there were any input errors from prior to the user clicking "save"
+if ($input_errors) { 
+	print_input_errors($input_errors);
+}
+?>
 
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
 	<tr><td class="tabnavtbl">
@@ -260,7 +265,6 @@ include("fbegin.inc");?>
 	</td></tr>
 	<tr><form action="extensions_thebrig_tools.php" method="post" name="iform" id="iform">
 		<td class="tabcont">
-			 <?php if (!empty($input_errors)) print_input_errors($input_errors); ?>
 			 <table width="100%" border="0" cellpadding="6" cellspacing="0">
 			 
 			 	<?php html_titleline(gettext("Configuration Backup/Restore"));?>
